1d7e9f1a127ef09deb0e904f9ae18b1395554d2e
VIJAY PATIL <vijaypatil90816@gmail.com>
2025-08-21 23:42:25 +0530

    DAY 7 : Problem Solved !!
    DAY 7 : Problem Solved !!

---
+++ Problem7.java
@@ -0,0 +1,46 @@
+/*Day 7 of 30 🗓
+
+Problem : Trapping Rain Water
+You are given an array height[] of non-negative integers where each element represents the height of a bar in a histogram-like structure. These bars are placed next to each other, and the width of each bar is 1 unit. When it rains, water gets trapped between the bars if there are taller bars on both the left and right of the shorter bars. The task is to calculate how much water can be trapped between these bars after the rain.
+
+Input :
+An integer array height[] where each element represents the height of a bar in the histogram.
+Example : 
+height[] = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+ 
+ */
+
+public class Problem7 {
+
+    public static void main(String[] args) {
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println("Trapped water = " + trap(height)); // Output: 6
+
+    }
+
+    public static int trap(int[] height) {
+        int n = height.length;
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int waterTrapped = 0;
+
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    waterTrapped += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    waterTrapped += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return waterTrapped;
+    }
+}
